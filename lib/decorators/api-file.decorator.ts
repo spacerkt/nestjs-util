9a6d223574fdece809e7a60c9c18219dd2340c2b
José Cleiton <jcleitonbc@gmail.com>
2020-09-10 21:59:12 -0300

    feat(decorators): api file with config parameters

---
+++ lib/decorators/api-file.decorator.ts
@@ -0,0 +1,29 @@
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { applyDecorators } from '@nestjs/common';
+
+const File = (filename = 'file', description?: string): MethodDecorator => (
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor,
+) => {
+  ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        [filename]: {
+          type: 'string',
+          format: 'binary',
+          description: description ?? 'Binary file sended by form data',
+        },
+      },
+    },
+  })(target, propertyKey, descriptor);
+};
+
+export function ApiFile(field = 'file', description?: string) {
+  return applyDecorators(
+    File(field, description),
+    ApiConsumes('multipart/form-data'),
+  );
+}
+
---
+++ lib/decorators/index.ts
@@ -0,0 +1,2 @@
+export * from './api-file.decorator';
+
