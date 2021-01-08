467d511a58d5225c3548fb73e408ed6ade7a3784
José Cleiton <jcleitonbc@gmail.com>
2021-01-08 07:31:29 -0300

    feat(test): initDatabase

---
+++ lib/typeorm/testing/initTestingDatabase.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { loadFixtures } from './utils';
+
+export async function initTestDatabase(
+  app: INestApplication,
+  path: string,
+): Promise<Connection | undefined> {
+  if (process.env.NODE_ENV !== 'test') {
+    return undefined;
+  }
+  const connection = app.get<Connection>(Connection);
+  if (!connection) {
+    return undefined;
+  }
+  await connection.synchronize(true);
+  await loadFixtures(path, connection);
+  return connection;
+}
